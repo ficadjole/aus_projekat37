8897f7828fa648173dd8173ff6cfd6b21f679a3a
MajaBogicevic <maja.bogicevic01@gmail.com>
2025-09-04 20:47:19 +0200

    3

--- dCom/Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
+++ dCom/Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
@@ -24,15 +24,48 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters readParams = (ModbusWriteCommandParameters)CommandParameters;
+
+            byte[] request = new byte[12];
+            int offset = 0;
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)readParams.TransactionId)), 0, request,0, 2);
+            offset += 2;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)readParams.ProtocolId)), 0, request,0, 2);
+            offset += 2;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)readParams.Length)), 0, request,4, 2);
+            offset += 2;
+            request[offset++] = readParams.UnitId;
+            request[offset++] = readParams.FunctionCode;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)readParams.OutputAddress)), 0, request, 8, 2);
+            offset += 2;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)readParams.Value)), 0, request, 10, 2);
+            offset += 2;
+
+            return request;
+            
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            Dictionary<Tuple<PointType, ushort>, ushort> dictionary = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            byte[] tempAddress = new byte[2];
+            byte[] tempValue = new byte[2];
+
+            tempAddress[0] = response[8];
+            tempValue[1] = response[9];
+
+            ushort address = BitConverter.ToUInt16(new byte[2] { tempAddress[1], tempAddress[0] }, 0);
+            tempValue[0] = response[10];
+            tempValue[1] = response[11];
+
+            ushort value = BitConverter.ToUInt16(new byte[2] { tempValue[1], tempValue[0] }, 0);
+
+            dictionary.Add(new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, address), value);
+
+            return dictionary;
         }
     }
 }
