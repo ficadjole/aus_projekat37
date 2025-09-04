afa9959bd72794a512da2c6c14cc4ecfbf6977bf
MajaBogicevic <maja.bogicevic01@gmail.com>
2025-09-04 21:04:01 +0200

    egu conv

--- dCom/ProcessingModule/ProcessingManager.cs
+++ dCom/ProcessingModule/ProcessingManager.cs
@@ -77,7 +77,8 @@ namespace ProcessingModule
         /// <param name="value">The value.</param>
         private void ExecuteAnalogCommand(IConfigItem configItem, ushort transactionId, byte remoteUnitAddress, ushort pointAddress, int value)
         {
-            ModbusWriteCommandParameters p = new ModbusWriteCommandParameters(6, (byte)ModbusFunctionCode.WRITE_SINGLE_REGISTER, pointAddress, (ushort)value, transactionId, remoteUnitAddress);
+            ushort rowValue = eguConverter.ConvertToRaw(configItem.ScaleFactor, configItem.Deviation, value);
+            ModbusWriteCommandParameters p = new ModbusWriteCommandParameters(6, (byte)ModbusFunctionCode.WRITE_SINGLE_REGISTER, pointAddress, rowValue, transactionId, remoteUnitAddress);
             IModbusFunction fn = FunctionFactory.CreateModbusFunction(p);
             this.functionExecutor.EnqueueCommand(fn);
         }
@@ -142,6 +143,7 @@ namespace ProcessingModule
         {
             point.RawValue = newValue;
             point.Timestamp = DateTime.Now;
+            point.EguValue = eguConverter.ConvertToEGU(point.ConfigItem.ScaleFactor, point.ConfigItem.Deviation, point.RawValue);
         }
 
         /// <inheritdoc />
